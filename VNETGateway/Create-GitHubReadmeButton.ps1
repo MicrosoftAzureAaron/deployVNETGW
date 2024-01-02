05e5d68bb2536b714aa25496811065950530c4b6
Azure Aaron <aarosanders@microsoft.com>
2024-01-02 17:43:37 -0600

    getting fancy

---
+++ VNETGateway/Create-GitHubReadmeButton.ps1
@@ -0,0 +1,19 @@
+param(
+    [string]$BranchName = "main",
+    [string]$DirectoryName
+)
+
+$originalURL = "https://github.com/MicrosoftAzureAaron/deployVNETGW/blob/${BranchName}${DirectoryName}/src/main.json"
+$removeBlob = $originalURL.Remove($originalURL.IndexOf("/blob"), 5)
+$shortURL = $removeBlob.Substring(14)
+$rawURL = "https://raw.githubusercontent${shortURL}"
+$encodedURL = [uri]::EscapeDataString($rawURL)
+
+# Specify the path to the readme.md file
+$readmePath = "readme.md"
+
+# Content to append to the readme.md file
+$newContent = "[![Deploy to Azure](https://aka.ms/deploytoazurebutton)](https://portal.azure.com/#create/Microsoft.Template/uri/${encodedURL})"
+
+# Append content to the readme.md file
+set-Content -Path $readmePath -Value $newContent
