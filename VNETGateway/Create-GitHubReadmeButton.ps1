7896e4b4a4ad689eb289adfc5a4a7695d666aa63
Azure Aaron <aarosanders@microsoft.com>
2024-01-02 19:21:46 -0600

    buttontesting

--- VNETGateway/Create-GitHubReadmeButton.ps1
+++ VNETGateway/Create-GitHubReadmeButton.ps1
@@ -1,4 +1,4 @@
-$originalURL = "https://raw.githubusercontent.com/MicrosoftAzureAaron/deployVNETGW/main/VNETGateway/src/main.json"
+$originalURL = "https://github.com/MicrosoftAzureAaron/deployVNETGW/blob/7711e56f944f0d3b483db742abb991eabca33bc2/VNETGateway/src/main.json"
 $removeBlob = $originalURL.Remove($originalURL.IndexOf("/blob"), 5)
 $shortURL = $removeBlob.Substring(14)
 $rawURL = "https://raw.githubusercontent${shortURL}"
